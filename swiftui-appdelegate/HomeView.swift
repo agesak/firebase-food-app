6244d4dfbf6c7a638b83374f38d07b4f371836b7
Kareha Agesa <kmaagesa@gmail.com>
2021-01-27 16:37:10 -0800

    simple state with sign up

--- swiftui-appdelegate/HomeView.swift
+++ swiftui-appdelegate/HomeView.swift
@@ -9,49 +9,11 @@ import SwiftUI
 import Firebase
 
 
-
-
-
-
-
-struct HomeView: View {
-    @State var showMenu = false
-    @State var signUp = false
+struct HomeView : View {
     
-    var body: some View {
-        
-        return NavigationView {
-            
-            
-            GeometryReader { geometry in
-                ZStack(alignment: .leading) {
-                    MainView(showMenu: self.$showMenu, signUp: self.$signUp)
-                        .frame(width: geometry.size.width, height: geometry.size.height)
-                        .offset(x: self.showMenu ? geometry.size.width/2 : 0)
-                        .disabled(self.showMenu ? true : false)
-                    if self.showMenu {
-                        MenuView()
-                            .frame(width: geometry.size.width/2)
-                            .transition(.move(edge: .leading))
-                    }
-                    if self.signUp {
-                        UserView()
-                        .frame(width: geometry.size.width, height: geometry.size.height)
-                    }
-                }
-            }
-            .navigationBarItems(leading: (
-                                Button(action: {
-                                    withAnimation {
-                                        self.showMenu.toggle()
-                                    }
-                                }) {
-                                    Image(systemName: "line.horizontal.3")
-                                        .imageScale(.large)
-                                }
-                            ))
-            }
-        }
+    var body : some View {
+        MainView()
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -61,75 +23,167 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct MainView: View {
+struct MainView : View {
     
-    @Binding var showMenu: Bool
-    @Binding var signUp: Bool
-    @Environment(\.colorScheme) var colorScheme: ColorScheme
-    
-    var body: some View {
-
-  
-        ZStack {
-            if colorScheme == .dark {
-                Image("barbell_2nd_lighter").resizable().aspectRatio(contentMode: .fill).ignoresSafeArea()
-            } else {
-                Image("barbell").resizable().aspectRatio(contentMode: .fill).ignoresSafeArea().opacity(0.1)
-            }
+    var body : some View {
+        VStack {
+            Spacer()
+            AppName()
+            Spacer()
+            AppLogo()
+            Spacer()
             
-            VStack {
-                Spacer()
-                AppName()
-                Spacer()
-                AppLogo()
-                Spacer()
-                
-//                cant figure out how to make function work
-//                CreateAccount()
-                Button(action: {
-                    withAnimation {
-                        self.signUp.toggle()}
-                }) {
-                    Text("Sign Up")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .padding()
-                      .frame(width: 250, height: 50)
-        //                for custom divide rbg by 255
-                        .background(Color.purple)
-                      .cornerRadius(10.0)
-                }
-                Spacer().frame(height: 30)
-                SignIn()
-                Spacer()
-            }
+            SignUp()
+//            Button(action: {
+//                print("Sign Up")
+////                withAnimation {
+////                    self.signUp.toggle()}
+//            }) {
+//                Text("Sign Up")
+//                    .font(.title)
+//                    .fontWeight(.bold)
+//                    .foregroundColor(.white)
+//                    .padding()
+//                  .frame(width: 250, height: 50)
+//    //                for custom divide rbg by 255
+//                    .background(Color.purple)
+//                  .cornerRadius(10.0)
+//            }
+         
+            SignIn()
         }
     }
 }
 
 
-//struct CreateAccount: View {
-//
-//    var signUp: Bool
+
+//struct HomeView: View {
+//    @State var showMenu = false
+//    @State var signUp = false
 //
 //    var body: some View {
-//        Button(action: {
-//            withAnimation {
-//                signUp.toggle()}
-//        }) {
-//            Text("Sign Up")
-//                .font(.title)
-//                .fontWeight(.bold)
-//                .foregroundColor(.white)
-//                .padding()
-//              .frame(width: 250, height: 50)
-////                for custom divide rbg by 255
-//                .background(Color.purple)
-//              .cornerRadius(10.0)
+//
+//        return NavigationView {
+//
+//
+//            GeometryReader { geometry in
+//                ZStack(alignment: .leading) {
+//                    MainView(showMenu: self.$showMenu, signUp: self.$signUp)
+//                        .frame(width: geometry.size.width, height: geometry.size.height)
+//                        .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+//                        .disabled(self.showMenu ? true : false)
+//                    if self.showMenu {
+//                        MenuView()
+//                            .frame(width: geometry.size.width/2)
+//                            .transition(.move(edge: .leading))
+//                    }
+//                    if self.signUp {
+//                        UserView()
+//                        .frame(width: geometry.size.width, height: geometry.size.height)
+//                    }
+//                }
+//            }
+//            .navigationBarItems(leading: (
+//                                Button(action: {
+//                                    withAnimation {
+//                                        self.showMenu.toggle()
+//                                    }
+//                                }) {
+//                                    Image(systemName: "line.horizontal.3")
+//                                        .imageScale(.large)
+//                                }
+//                            ))
+//            }
+//        }
+//}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HomeView()
+//
+//    }
+//}
+
+//struct MainView: View {
+//
+//    @Binding var showMenu: Bool
+//    @Binding var signUp: Bool
+//    @Environment(\.colorScheme) var colorScheme: ColorScheme
+//
+//    var body: some View {
+//
+//
+//        ZStack {
+//            if colorScheme == .dark {
+//                Image("barbell_2nd_lighter").resizable().aspectRatio(contentMode: .fill).ignoresSafeArea()
+//            } else {
+//                Image("barbell").resizable().aspectRatio(contentMode: .fill).ignoresSafeArea().opacity(0.1)
+//            }
+//
+//            VStack {
+//                Spacer()
+//                AppName()
+//                Spacer()
+//                AppLogo()
+//                Spacer()
+//
+////                cant figure out how to make function work
+////                CreateAccount()
+//                Button(action: {
+//                    withAnimation {
+//                        self.signUp.toggle()}
+//                }) {
+//                    Text("Sign Up")
+//                        .font(.title)
+//                        .fontWeight(.bold)
+//                        .foregroundColor(.white)
+//                        .padding()
+//                      .frame(width: 250, height: 50)
+//        //                for custom divide rbg by 255
+//                        .background(Color.purple)
+//                      .cornerRadius(10.0)
+//                }
+//                Spacer().frame(height: 30)
+//                SignIn()
+//                Spacer()
+//            }
 //        }
 //    }
 //}
+
+
+struct SignUp: View {
+
+//    var signUp: Bool
+    
+    @State var signUp = false
+
+    var body: some View {
+        Button(action: {
+//            print("Sign Up")
+            withAnimation {
+                self.signUp.toggle()
+                
+                if signUp {
+                    print("signed up")
+                } else {
+                    print("not signed up")
+                }
+                
+            }
+        }) {
+            Text("Sign Up")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding()
+              .frame(width: 250, height: 50)
+//                for custom divide rbg by 255
+                .background(Color.purple)
+              .cornerRadius(10.0)
+        }
+    }
+}
 
 struct SignIn: View {
     var body: some View {
